9f18fcf668c16ffc50d6792f13c4a08e4a59ce25
yuki <123syokutaku@gmail.com>
2017-02-04 18:52:08 +0900

    NFA完成

--- Automata.py
+++ Automata.py
@@ -18,3 +18,29 @@ class DeterministicFiniteAutomata(object):
             else:
                 state = self.transitions[state][char]
         return state in self.final_states
+
+
+class NondeterministicFiniteAutomata(object):
+    """ 非決定性有限オートマトン """
+
+    def __init__(self, states, alphabets, transitions, init_state, final_states):
+        self.states = states
+        self.alphabets = alphabets
+        self.transitions = transitions
+        self.init_state = init_state
+        self.final_states = final_states
+
+    def run(self, string):
+        """ stringを認識するかチェックする """
+        states = {self.init_state}
+        for char in string:
+            if char not in self.alphabets:
+                print("ERROR : invalid character \"%s\"" % char)
+                return False
+            else:
+                new_states = set()
+                for state in states:
+                    if self.transitions[state].get(char):
+                        new_states = new_states.union(self.transitions[state][char])
+                states = new_states
+        return len(states.intersection(self.final_states)) != 0
--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from Automata import DeterministicFiniteAutomata as DFA
+from Automata import NondeterministicFiniteAutomata as NFA
 
 
 def is_length_even():
     """ 長さが偶数の文字列を認識 """
     print("is_length_even".center(50, "-"))
-    a = hash("a")
-    b = hash("b")
+    a, b = range(2)
     states = [a, b]
     alphabets = "01"
     transitions = {
@@ -21,17 +21,15 @@ def is_length_even():
 
     for test in tests:
         if automata.run(test):
-            print("length even : \"%s\"" % test)
+            print("accept : \"%s\"" % test)
         else:
-            print("length odd  : \"%s\"" % test)
+            print("reject : \"%s\"" % test)
 
 
-def is_mod_three():
+def is_divisible_by_3():
     """ 二進表記で3の倍数の文字列を認識 """
-    print("is_mod_three".center(50, "-"))
-    a = hash("a")
-    b = hash("b")
-    c = hash("c")
+    print("is_divisible_by_3".center(50, "-"))
+    a, b, c = range(3)
     states = [a, b, c]
     alphabets = "01"
     transitions = {
@@ -48,11 +46,61 @@ def is_mod_three():
     tests = ["00000", "0011", "01110", "1001", "1110101110"]
     for test in tests:
         if automata.run(test):
-            print("mod 3 in binary     : \"%s\"" % test)
+            print("accept : \"%s\"" % test)
         else:
-            print("not mod 3 in binary : \"%s\"" % test)
+            print("reject : \"%s\"" % test)
 
+
+def is_end_0XXX():
+    """ 最後から四文字目が0である文字列を認識 """
+    print("is_end_0XXX".center(50, "-"))
+    a, b, c, d, e = range(5)
+    states = [a, b, c, d, e]
+    alphabets = "01"
+    transitions = {
+        a: {"0": {a, b}, "1": {a}},
+        b: {"0": {c}, "1": {c}},
+        c: {"0": {d}, "1": {d}},
+        d: {"0": {e}, "1": {e}},
+        e: {},
+    }
+    init_state = a
+    final_states = {e}
+    automata = NFA(states, alphabets, transitions, init_state, final_states)
+
+    tests = ["00000000", "000110", "1110", "01", "1110100110"]
+    for test in tests:
+        if automata.run(test):
+            print("accept : \"%s\"" % test)
+        else:
+            print("reject : \"%s\"" % test)
+
+
+def has_010():
+    """ 010を部分列に持つ文字列を認識 """
+    print("has_010".center(50, "-"))
+    a, b, c, d = range(4)
+    states = [a, b, c, d]
+    alphabets = "01"
+    transitions = {
+        a: {"0": {a, b}, "1": {a}},
+        b: {"1": {c}},
+        c: {"0": {d}},
+        d: {"0": {d}, "1": {d}},
+    }
+    init_state = a
+    final_states = {d}
+    automata = NFA(states, alphabets, transitions, init_state, final_states)
+
+    tests = ["010", "000110", "111111001010", "111101", "1110100110"]
+    for test in tests:
+        if automata.run(test):
+            print("accept : \"%s\"" % test)
+        else:
+            print("reject : \"%s\"" % test)
 
 is_length_even()
-is_mod_three()
+is_divisible_by_3()
+is_end_0XXX()
+has_010()
 print("program ended.")
