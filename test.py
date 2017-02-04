749becfe115b60c272f8585da32616f788147a33
yuki <123syokutaku@gmail.com>
2017-02-04 22:57:44 +0900

    unittestを使ってテストを書き直した

--- test.py
+++ test.py
@@ -1,133 +1,142 @@
+import unittest
 from Automata import DeterministicFiniteAutomata as DFA
 from Automata import NondeterministicFiniteAutomata as NFA
 from Automata import NFAWithEpsilonTransition as eNFA
 
 
-def is_length_even():
+class IsLengthEven(DFA):
     """ 長さが偶数の文字列を認識 """
-    print("is_length_even".center(50, "-"))
-    a, b = range(2)
-    states = [a, b]
-    alphabets = "01"
-    transitions = {
-        a: {"0": b, "1": b},
-        b: {"0": a, "1": a},
-    }
+    tests = (("0111", True),
+             ("1", False),
+             ("", True),
+             ("0011011", False))
 
-    init_state = a
-    final_states = {a}
-    automata = DFA(states, alphabets, transitions, init_state, final_states)
-
-    tests = ["0111", "1", "", "0011011"]
-
-    for test in tests:
-        if automata.run(test):
-            print("accept : \"%s\"" % test)
-        else:
-            print("reject : \"%s\"" % test)
+    def __init__(self):
+        a, b = range(2)
+        states = [a, b]
+        alphabets = "01"
+        transitions = {
+            a: {"0": b, "1": b},
+            b: {"0": a, "1": a},
+        }
+        init_state = a
+        final_states = {a}
+        super().__init__(states, alphabets, transitions, init_state, final_states)
 
 
-def is_divisible_by_3():
+class IsDivisibleBy3(DFA):
     """ 二進表記で3の倍数の文字列を認識 """
-    print("is_divisible_by_3".center(50, "-"))
-    a, b, c = range(3)
-    states = [a, b, c]
-    alphabets = "01"
-    transitions = {
-        a: {"0": a, "1": b},
-        b: {"0": c, "1": a},
-        c: {"0": b, "1": c},
-    }
+    tests = (("00000", True),       # 0
+             ("0011", True),        # 3
+             ("01110", False),      # 14
+             ("1001", True),        # 9
+             ("1110101110", True))  # 942
 
-    init_state = a
-    final_states = {a}
-    automata = DFA(states, alphabets, transitions, init_state, final_states)
+    def __init__(self):
+        a, b, c = range(3)
+        states = [a, b, c]
+        alphabets = "01"
+        transitions = {
+            a: {"0": a, "1": b},
+            b: {"0": c, "1": a},
+            c: {"0": b, "1": c},
+        }
 
-    # 0, 3, 14, 9, 942
-    tests = ["00000", "0011", "01110", "1001", "1110101110"]
-    for test in tests:
-        if automata.run(test):
-            print("accept : \"%s\"" % test)
-        else:
-            print("reject : \"%s\"" % test)
+        init_state = a
+        final_states = {a}
+        super().__init__(states, alphabets, transitions, init_state, final_states)
 
 
-def is_end_0XXX():
+class IsEnd0XXX(NFA):
     """ 最後から四文字目が0である文字列を認識 """
-    print("is_end_0XXX".center(50, "-"))
-    a, b, c, d, e = range(5)
-    states = [a, b, c, d, e]
-    alphabets = "01"
-    transitions = {
-        a: {"0": {a, b}, "1": {a}},
-        b: {"0": {c}, "1": {c}},
-        c: {"0": {d}, "1": {d}},
-        d: {"0": {e}, "1": {e}},
-        e: {},
-    }
-    init_state = a
-    final_states = {e}
-    automata = NFA(states, alphabets, transitions, init_state, final_states)
+    tests = (("00000000", True),
+             ("000110", True),
+             ("1110", False),
+             ("01", False),
+             ("1110100110", True))
 
-    tests = ["00000000", "000110", "1110", "01", "1110100110"]
-    for test in tests:
-        if automata.run(test):
-            print("accept : \"%s\"" % test)
-        else:
-            print("reject : \"%s\"" % test)
+    def __init__(self):
+        a, b, c, d, e = range(5)
+        states = [a, b, c, d, e]
+        alphabets = "01"
+        transitions = {
+            a: {"0": {a, b}, "1": {a}},
+            b: {"0": {c}, "1": {c}},
+            c: {"0": {d}, "1": {d}},
+            d: {"0": {e}, "1": {e}},
+            e: {},
+        }
+        init_state = a
+        final_states = {e}
+        super().__init__(states, alphabets, transitions, init_state, final_states)
 
 
-def has_010():
+class Has010(NFA):
     """ 010を部分列に持つ文字列を認識 """
-    print("has_010".center(50, "-"))
-    a, b, c, d = range(4)
-    states = [a, b, c, d]
-    alphabets = "01"
-    transitions = {
-        a: {"0": {a, b}, "1": {a}},
-        b: {"1": {c}},
-        c: {"0": {d}},
-        d: {"0": {d}, "1": {d}},
-    }
-    init_state = a
-    final_states = {d}
-    automata = NFA(states, alphabets, transitions, init_state, final_states)
+    tests = (("010", True),
+             ("000110", False),
+             ("111111001010", True),
+             ("111101", False),
+             ("1110100110", True))
 
-    tests = ["010", "000110", "111111001010", "111101", "1110100110"]
-    for test in tests:
-        if automata.run(test):
-            print("accept : \"%s\"" % test)
-        else:
-            print("reject : \"%s\"" % test)
+    def __init__(self):
+        a, b, c, d = range(4)
+        states = [a, b, c, d]
+        alphabets = "01"
+        transitions = {
+            a: {"0": {a, b}, "1": {a}},
+            b: {"1": {c}},
+            c: {"0": {d}},
+            d: {"0": {d}, "1": {d}},
+        }
+        init_state = a
+        final_states = {d}
+        super().__init__(states, alphabets, transitions, init_state, final_states)
 
 
-def is_increasing_sequence():
+class IsIncreasingSequence(eNFA):
     """ 広義単調増加な文字列を認識 """
-    print("is_increasing_sequence".center(50, "-"))
-    a, b, c, d, e = range(5)
-    states = [a, b, c, d, e]
-    alphabets = "01234"
-    transitions = {
-        a: {"0": {a}, -1: {b}},
-        b: {"1": {b}, -1: {c}},
-        c: {"2": {c}, -1: {d}},
-        d: {"3": {d}, -1: {e}},
-        e: {"4": {e}}
-    }
-    init_state = a
-    final_states = {e}
-    automata = eNFA(states, alphabets, transitions, init_state, final_states)
+    tests = (("01234", True),
+             ("01122334444", True),
+             ("01234231", False),
+             ("1111", True),
+             ("1133344", True))
 
-    tests = ["01234", "01122334444", "01234231", "1111", "1133344"]
-    for test in tests:
-        if automata.run(test):
-            print("accept : \"%s\"" % test)
-        else:
-            print("reject : \"%s\"" % test)
+    def __init__(self):
+        a, b, c, d, e = range(5)
+        states = [a, b, c, d, e]
+        alphabets = "01234"
+        transitions = {
+            a: {"0": {a}, -1: {b}},
+            b: {"1": {b}, -1: {c}},
+            c: {"2": {c}, -1: {d}},
+            d: {"3": {d}, -1: {e}},
+            e: {"4": {e}}
+        }
+        init_state = a
+        final_states = {e}
+        super().__init__(states, alphabets, transitions, init_state, final_states)
 
-is_length_even()
-is_divisible_by_3()
-is_end_0XXX()
-has_010()
-is_increasing_sequence()
-print("program ended.")
+
+class AutomatonTest(unittest.TestCase):
+    """ オートマトンの動作確認 """
+    automaton = [
+        IsLengthEven,
+        IsDivisibleBy3,
+        IsEnd0XXX,
+        Has010,
+        IsIncreasingSequence
+    ]
+
+    def test_automaton(self):
+        """ テスト本体 """
+        for automata in self.automaton:
+            print(str(automata)[17:-2].center(70, "-"))
+            instance = automata()
+            for string, expected in instance.tests:
+                print("IN : \"%s\"\nOUT: %s" % (string, str(expected)))
+                result = instance.run(string)
+                self.assertEqual(expected, result)
+
+if __name__ == "__main__":
+    unittest.main()
