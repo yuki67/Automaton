6470add18f4f812017cd381e1d70db4ae5cbb20e
yuki <123syokutaku@gmail.com>
2017-03-06 19:21:38 +0900

    連結に対するテスト追加

--- RegexTest.py
+++ RegexTest.py
@@ -5,10 +5,19 @@ from Regex import regex_to_eNFA
 class RegexTest(unittest.TestCase):
 
     def runTest(self):
-        alphabet = "ab"
-        cases = [["a", [["a", True], ["b", False], ["aa", False], ["ab", False]]]]
+        alphabet = {"a", "b"}
+        cases = [["a",
+                  [["a", True],
+                   ["b", False],
+                   ["aa", False],
+                   ["ab", False]]],
+                 ["aba",
+                  [["aba", True],
+                   ["aaa", False],
+                   ["baa", False]]]]
         for regex, tests in cases:
             enfa = regex_to_eNFA(regex, alphabet)
+            print(enfa)
             for test, expected in tests:
                 self.assertEqual(enfa.run(test), expected)
 
